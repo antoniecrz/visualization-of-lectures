0fe3131b4107581a0ecd048acc5282675c70e13b
Austin <47179017+austcollins@users.noreply.github.com>
2021-03-11 17:03:26 +0000

    Added TODOs

--- api/index.js
+++ api/index.js
@@ -5,6 +5,18 @@ const express = require('express');
 const api = express.Router();
 
 api.get('/', async (req, res) => {
+
+
+    // TODO: get text from browser client
+
+    // TODO: run through natural language processor
+
+    // TODO: search in google API to get images from trusted sources
+
+    // TODO: return keywords used
+
+    // TODO: return links to images
+
     res.send('123');
     // example api route
 });
